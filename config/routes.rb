da77ad4211cdae9a5aa1b73a2fa38c4429497cda
Kylie <kylie.rae.slattery@gmail.com>
2019-11-15 16:16:41 +0000

    added contact and volunteer static pages and hooked up links, fixed font to display for entire app

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'recycleable_items', to: 'conservations#recycleable_items'
   get 'how_to_compost', to: 'conservations#how_to_compost'
   get 'references', to: 'conservations#references'
+  get 'volunteer', to: 'conservations#volunteer'
+  get 'contact', to: 'conservations#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
