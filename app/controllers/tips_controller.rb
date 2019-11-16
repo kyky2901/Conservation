e8bdfdb22830d0bd06356ee0ef2b9e727d9d2b3e
Kylie <kylie.rae.slattery@gmail.com>
2019-11-16 19:36:14 +0000

    added tip params method, and link to new tip simple form. added validation rules in tip model

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -2,4 +2,22 @@ class TipsController < ApplicationController
   def index
     @tip = Tip.order("RANDOM()").first
   end
+
+  def new
+    @tip = Tip.new
+  end
+
+  def create 
+    @tip = Tip.create(tip_params)
+    if @tip.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to tips_path
+  end
+
+  private
+
+  def tip_params
+    params.require(:tip).permit(:text, :source)
+  end
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,4 @@
 class Tip < ApplicationRecord
+  validates :text, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :source, presence: true, length: { maximum: 100, minimum: 3 }
 end
